3a89ddfe44903509525d058fb19f81aca1776845
Marchuk Roman <marchukroman228@gmail.com>
2020-10-10 20:06:27 +0300

    final design changes:

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,6 @@ const headerTextUl = document.getElementById('header-text-ul');
 
 hamburger.addEventListener('click', () => {
     headerTextUl.classList.toggle('show');
+    wrapper.classList.toggle('show');
+    main__header.classList.toggle('show');
 });
